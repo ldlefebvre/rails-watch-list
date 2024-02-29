14afe652f7a8e179e909b92ca061b9fd616a8654
Laurent Lefebvre <laurentlefebvre8@hotmail.com>
2024-02-29 16:38:59 -0500

    create the bookmarks routes, controller and view

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,34 @@
 class BookmarksController < ApplicationController
+  before_action :set_bookmark, only: %i[ destroy ]
+
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark.destroy!
+    redirect_to list_path(@bookmark.list)
+  end
+
+  private
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:movie_id, :comment) # Adjust attributes as needed
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  resources :lists, only: [:index, :show, :new, :create]
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+
+  resources :bookmarks, only: [:destroy]
+
   get "up" => "rails/health#show", as: :rails_health_check
-  # A user can see all the lists
-  # GET "lists"
-  # A user can see the details of a given list and its name
-  # GET "lists/42"
-  # A user can create a new list
-  # GET "lists/new"
-  # POST "lists"
 end
 
 
